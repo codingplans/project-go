ad11de8519b3254506b6862047100cdbafbcbb92
darrenzzy <zzyphp@gmail.com>
2021-05-10 21:21:47 +0800

    [每日一题] 124 新 case 复盘

--- leetcode/124/code_test.go
+++ leetcode/124/code_test.go
@@ -18,6 +18,7 @@ var NULL = -1 << 63
 var tests = []test{
 	{Lists: []int{-10, 9, 20, NULL, NULL, 15, 7}},
 	{Lists: []int{1, 2, 3}},
+	{Lists: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}},
 	{Lists: []int{1, -2, 3}},
 }
 
