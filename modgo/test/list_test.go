3e864805c80a69d245dbccf809b146c0bc78e003
darren <zzyphp@gmail.com>
2021-05-23 00:31:12 +0800

    [update] go mod

--- modgo/test/list_test.go
+++ modgo/test/list_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Darrenzzy/testgo/structures"
 	"testing"
 )
@@ -10,8 +9,10 @@ func TestReverse(t *testing.T) {
 
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
 	list := structures.Ints2List(arr)
-	list = Reverse(list)
-	fmt.Println(structures.List2Ints(list))
+	structures.Travel(list)
+	// list = Reverse(list)
+	//
+	// fmt.Println(structures.List2Ints(list))
 
 }
 
