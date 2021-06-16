36f3d581a1dea4274c3b278195e7e0f0219ad15a
darrenzzy <zzyphp@gmail.com>
2021-06-16 22:18:13 +0800

    update:初始化结构

--- leetcode/000_init_code/code_test.go
+++ leetcode/000_init_code/code_test.go
@@ -1,9 +1,30 @@
 package _00_init_code
 
 import (
+	"fmt"
 	"testing"
 )
 
+type test struct {
+	IntEs []int
+	Lists []int
+	K     int
+	K2    int
+	Str   string
+	Str2  string
+}
+
+var tests = []test{}
+
+//
+
 func Test_upToDayUp(t *testing.T) {
+	for _, v := range tests {
+		fmt.Println("初始化")
+
+		fmt.Println(v.IntEs)
+		pre := 1
+		fmt.Println("结果：", pre)
+	}
 
 }
