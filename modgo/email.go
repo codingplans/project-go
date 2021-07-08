b3bea9efa56e98ba7186f3706ecb6466693a2064
darrenzzy <zzyphp@gmail.com>
2021-07-09 00:24:32 +0800

    updata : 去敏感信息

--- modgo/email.go
+++ modgo/email.go
@@ -9,14 +9,14 @@ import (
 func TestExampleGmail() {
 	println(222)
 	e := email.NewEmail()
-	e.From = "zzyphp@gmail.com"
-	e.To = []string{"darrenzzy@126.com"}
+	e.From = "wwwwww@gmail.com"
+	e.To = []string{"wwwwwww@126.com"}
 	// e.Bcc = []string{"darrenzzy@126.com"}
 	// e.Cc = []string{"darrenzzy@126.com"}
 	e.Subject = "Awesome Subject"
 	e.Text = []byte("Text Body is, of course, supported!\n")
 	e.HTML = []byte("<h1>Fancy Html is supported, too!</h1>\n")
-	err := e.Send("smtp.gmail.com:587", smtp.PlainAuth("", e.From, "facaiba123", "smtp.gmail.com"))
+	err := e.Send("smtp.gmail.com:587", smtp.PlainAuth("", e.From, "12345678", "smtp.gmail.com"))
 	if err != nil {
 		log.Info(err.Error())
 
