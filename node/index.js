e053e9f2a238569b2f6c4cdb6e417d4ecf00dc70
darren911 <zzyphp@gmail.com>
2022-03-26 12:44:03 +0800

    set nul

--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
-  user     : 'root',
-  password : '123456',
+  user     : '',
+  password : '',
     port:23306,
   database : 'test'
 });
